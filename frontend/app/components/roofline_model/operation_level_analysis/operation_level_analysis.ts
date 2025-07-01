d4f999541ebedc52ab38360749128e54af9561b3
Profiler Team <copybara-servicebot@google.com>
2025-07-01 12:13:02 -0700

    Provide more context in the page.
    This change shows HLO Module and HLO Op at the top of the page.
    
    PiperOrigin-RevId: 778140180

--- frontend/app/components/roofline_model/operation_level_analysis/operation_level_analysis.ts
+++ frontend/app/components/roofline_model/operation_level_analysis/operation_level_analysis.ts
@@ -1,24 +1,15 @@
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnChanges,
-  OnInit,
-  Output,
-  SimpleChanges,
-} from '@angular/core';
+import {Component, ElementRef, EventEmitter, inject, Injector, Input, OnChanges, OnInit, Output, Renderer2, SimpleChanges, ViewChild} from '@angular/core';
 import {PIE_CHART_PALETTE} from 'org_xprof/frontend/app/common/constants/roofline_model_constants';
 import {ChartDataInfo} from 'org_xprof/frontend/app/common/interfaces/chart';
 import {SimpleDataTable} from 'org_xprof/frontend/app/common/interfaces/data_table';
 import {CategoryTableDataProcessor} from 'org_xprof/frontend/app/components/chart/category_table_data_processor';
-import {
-  PIE_CHART_OPTIONS,
-  SCATTER_CHART_OPTIONS,
-} from 'org_xprof/frontend/app/components/chart/chart_options';
+import {PIE_CHART_OPTIONS, SCATTER_CHART_OPTIONS} from 'org_xprof/frontend/app/components/chart/chart_options';
 import {Dashboard} from 'org_xprof/frontend/app/components/chart/dashboard/dashboard';
 import {DefaultDataProvider} from 'org_xprof/frontend/app/components/chart/default_data_provider';
+import {Table} from 'org_xprof/frontend/app/components/chart/table/table';
+import {SOURCE_CODE_SERVICE_INTERFACE_TOKEN} from 'org_xprof/frontend/app/services/source_code_service/source_code_service_interface';
 
-type ColumnIdxArr = Array<number | google.visualization.ColumnSpec>;
+type ColumnIdxArr = Array<number|google.visualization.ColumnSpec>;
 
 /**
  * An operation level analysis table view component (step appregation: total).
@@ -29,24 +20,22 @@ type ColumnIdxArr = Array<number | google.visualization.ColumnSpec>;
   templateUrl: './operation_level_analysis.ng.html',
   styleUrls: ['./operation_level_analysis.scss'],
 })
-export class OperationLevelAnalysis
-  extends Dashboard
-  implements OnInit, OnChanges
-{
+export class OperationLevelAnalysis extends Dashboard implements OnInit,
+                                                                 OnChanges {
+  private readonly injector = inject(Injector);
   /** The roofline model data, original dataset */
   // used for table chart and pie chart
-  @Input() rooflineModelData?: google.visualization.DataTable | null = null;
+  @Input() rooflineModelData?: google.visualization.DataTable|null = null;
   @Input() viewColumns: ColumnIdxArr = [];
   // data for scatter chart, heavey data preprocessing handled in parent
-  @Input() rooflineSeriesData?: google.visualization.DataTable | null = null;
+  @Input() rooflineSeriesData?: google.visualization.DataTable|null = null;
   @Input() scatterChartOptions: google.visualization.ScatterChartOptions = {};
   // Op name prepopulated from url
   @Input() selectedOp = '';
 
   @Output()
-  readonly filterUpdated = new EventEmitter<
-    google.visualization.DataTableCellFilter[]
-  >();
+  readonly filterUpdated =
+      new EventEmitter<google.visualization.DataTableCellFilter[]>();
 
   pieChartDataProvider = new DefaultDataProvider();
   scatterChartDataProvider = new DefaultDataProvider();
@@ -72,12 +61,54 @@ export class OperationLevelAnalysis
     options: SCATTER_CHART_OPTIONS,
   };
 
+  @ViewChild('table', {read: Table, static: false})
+  tableRef: Table|undefined = undefined;
+  @ViewChild('table', {read: ElementRef, static: false})
+  chartElementRef: ElementRef|undefined = undefined;
+  private readonly renderer: Renderer2 = inject(Renderer2);
+  stackTrace = '';
+  showStackTrace = false;
+  sourceCodeServiceIsAvailable = false;
+
   constructor() {
     super();
+    // We don't need the source code service to be persistently available.
+    // We temporarily use the service to check if it is available and show
+    // UI accordingly.
+    const sourceCodeService =
+        this.injector.get(SOURCE_CODE_SERVICE_INTERFACE_TOKEN, null);
+    this.sourceCodeServiceIsAvailable =
+        sourceCodeService?.isAvailable() === true;
+    if (this.sourceCodeServiceIsAvailable) {
+      this.addSourceInfoClickListener();
+    }
   }
 
   ngOnInit() {
     this.update();
+  }
+
+  private addSourceInfoClickListener() {
+    const chart = this.tableRef?.table;
+    const chartElement = this.chartElementRef?.nativeElement;
+    if (!chart || !chartElement) {
+      setTimeout(() => {
+        this.addSourceInfoClickListener();
+      }, 100);
+      return;
+    }
+    google.visualization.events.addListener(chart, 'ready', () => {
+      chartElement.querySelectorAll('.source-info-cell')
+          .forEach((element: Element) => {
+            this.renderer.listen(element, 'click', () => {
+              this.stackTrace = element.getAttribute('title') || '';
+            });
+          });
+    });
+  }
+
+  toggleShowStackTrace() {
+    this.showStackTrace = !this.showStackTrace;
   }
 
   ngOnChanges(changes: SimpleChanges) {
@@ -103,14 +134,14 @@ export class OperationLevelAnalysis
 
     // process data for pie chart
     this.pieChartDataProvider.parseData(
-      JSON.parse(this.dataTable.toJSON()) as SimpleDataTable,
+        JSON.parse(this.dataTable.toJSON()) as SimpleDataTable,
     );
     this.updateAndDrawPieCharts();
 
     // process data for roofline scatter chart
     if (this.rooflineSeriesData) {
       this.scatterChartDataProvider.parseData(
-        JSON.parse(this.rooflineSeriesData.toJSON()) as SimpleDataTable,
+          JSON.parse(this.rooflineSeriesData.toJSON()) as SimpleDataTable,
       );
       this.updateAndDrawScatterChart();
     }
@@ -139,7 +170,7 @@ export class OperationLevelAnalysis
     if (!this.dataTable) return;
     const opCategoryIndex = this.dataTable.getColumnIndex('category');
     const opTotalSelfTimeIndex =
-      this.dataTable.getColumnIndex('total_self_time');
+        this.dataTable.getColumnIndex('total_self_time');
     this.dataInfoCategoryPieChart.customChartDataProcessor =
         new CategoryTableDataProcessor(
             this.getFilters(),

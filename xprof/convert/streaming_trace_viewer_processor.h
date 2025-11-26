401237de4c5f09faa622279323f106536a7d27a8
Mudit Gokhale <muditgokhale@google.com>
2025-11-26 10:03:34 -0800

    Add support in trace_viewer to leverage the aggregator-worker setup
    PiperOrigin-RevId: 837167925

--- xprof/convert/streaming_trace_viewer_processor.h
+++ xprof/convert/streaming_trace_viewer_processor.h
@@ -13,29 +13,51 @@
 #include "plugin/xprof/protobuf/op_stats.pb.h"
 namespace xprof {
 
+namespace internal {
+// Options for trace viewer used for testing.
+struct TraceViewOption {
+  uint64_t resolution = 0;
+  double start_time_ms = 0.0;
+  double end_time_ms = 0.0;
+  std::string event_name = "";
+  std::string search_prefix = "";
+  double duration_ms = 0.0;
+  uint64_t unique_id = 0;
+};
+
+absl::StatusOr<TraceViewOption> GetTraceViewOption(
+    const tensorflow::profiler::ToolOptions& options);
+}  // namespace internal
+
 class StreamingTraceViewerProcessor : public ProfileProcessor {
  public:
   explicit StreamingTraceViewerProcessor(
-      const tensorflow::profiler::ToolOptions&) {}
+      const tensorflow::profiler::ToolOptions& options)
+      : options_(options) {}
 
   absl::Status ProcessSession(
       const tensorflow::profiler::SessionSnapshot& session_snapshot,
       const tensorflow::profiler::ToolOptions& options) final;
 
+  absl::StatusOr<std::string> Map(const std::string& xspace_path) override;
+
   absl::StatusOr<std::string> Map(
       const tensorflow::profiler::SessionSnapshot& session_snapshot,
       const std::string& hostname,
-      const tensorflow::profiler::XSpace& xspace) override {
-    return absl::UnimplementedError(
-        "Map not implemented for StreamingTraceViewerProcessor");
-  }
+      const tensorflow::profiler::XSpace& xspace) override;
 
   absl::Status Reduce(
       const tensorflow::profiler::SessionSnapshot& session_snapshot,
-      const std::vector<std::string>& map_output_files) override {
-    return absl::UnimplementedError(
-        "Reduce not implemented for StreamingTraceViewerProcessor");
+      const std::vector<std::string>& map_output_files) override;
+
+  bool ShouldUseWorkerService(
+      const tensorflow::profiler::SessionSnapshot& session_snapshot,
+      const tensorflow::profiler::ToolOptions& options) const override {
+    return session_snapshot.XSpaceSize() > 1;
   }
+
+ private:
+  tensorflow::profiler::ToolOptions options_;
 };
 
 }  // namespace xprof

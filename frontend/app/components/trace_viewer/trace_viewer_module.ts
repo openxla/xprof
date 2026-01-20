fd11e0d404507e17c131ac9cc8b8872261eb6750
Profiler Team <copybara-servicebot@google.com>
2026-01-20 10:38:00 -0800

    Introducing the trace viewer v2 through the trace viewer container component.
    PiperOrigin-RevId: 858656083

--- frontend/app/components/trace_viewer/trace_viewer_module.ts
+++ frontend/app/components/trace_viewer/trace_viewer_module.ts
@@ -1,13 +1,22 @@
+import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 import {PipesModule} from 'org_xprof/frontend/app/pipes/pipes_module';
 
 import {TraceViewer} from './trace_viewer';
+import {TraceViewerContainer} from './trace_viewer_container';
 
 /** A trace viewer module. */
 @NgModule({
-  declarations: [TraceViewer],
-  imports: [PipesModule],
-  exports: [TraceViewer]
+  declarations: [TraceViewer, TraceViewerContainer],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatProgressBarModule,
+    PipesModule,
+  ],
+  exports: [TraceViewer, TraceViewerContainer]
 })
 export class TraceViewerModule {
 }

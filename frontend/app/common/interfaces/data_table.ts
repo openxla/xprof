691b9c015031a17d9bf358f7935bd5453468a5d8
Profiler Team <copybara-servicebot@google.com>
2026-01-21 10:44:35 -0800

    Display disaggregated serving avg decoding step time on TPU in the XProf overview page
    PiperOrigin-RevId: 859169692

--- frontend/app/common/interfaces/data_table.ts
+++ frontend/app/common/interfaces/data_table.ts
@@ -277,6 +277,7 @@ export type OverviewPageDataTuple = [
   NormalizedAcceleratorPerformance,
   SimpleDataTable,
   PodViewerTopology,
+  SimpleDataTable,
 ];
 
 /* tslint:disable enforce-name-casing */

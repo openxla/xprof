e8c8997b200c07ca88f810b99ff8d2b5cae417a9
Profiler Team <copybara-servicebot@google.com>
2026-02-12 01:12:30 -0800

    Update the Trace Viewer V2 container with updated Trace Viewer V1 features
    PiperOrigin-RevId: 869083249

--- frontend/app/components/trace_viewer_container/trace_viewer_container.ts
+++ frontend/app/components/trace_viewer_container/trace_viewer_container.ts
@@ -1,11 +1,55 @@
 import {CommonModule} from '@angular/common';
-import {Component, ElementRef, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, EventEmitter, Input, OnDestroy, OnInit, Output, ViewChild} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 import {MatProgressBarModule} from '@angular/material/progress-bar';
-import {LOADING_STATUS_UPDATE_EVENT_NAME, TraceViewerV2LoadingStatus, type TraceViewerV2Module} from 'org_xprof/frontend/app/components/trace_viewer_v2/main';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatTableModule} from '@angular/material/table';
+import {isSearchEventsEvent, LOADING_STATUS_UPDATE_EVENT_NAME, SEARCH_EVENTS_EVENT_NAME, type SearchEventsEventDetail, TraceViewerV2LoadingStatus, type TraceViewerV2Module} from 'org_xprof/frontend/app/components/trace_viewer_v2/main';
 import {PipesModule} from 'org_xprof/frontend/app/pipes/pipes_module';
 import {interval, ReplaySubject, Subject, Subscription} from 'rxjs';
 import {debounceTime, takeUntil} from 'rxjs/operators';
+
+
+/**
+ * The name of the event selected custom event, dispatched from WASM in Trace
+ * Viewer v2.
+ */
+export const EVENT_SELECTED_EVENT_NAME = 'eventselected';
+
+/**
+ * The detail of an 'EntrySelected' custom event. The properties are quoted to
+ * prevent renaming during minification.
+ */
+export declare interface EntrySelectedEventDetail {
+  eventIndex: number;
+  name: string;
+  startUs: number;
+  durationUs: number;
+  startUsFormatted: string;
+  durationUsFormatted: string;
+  pid?: number;
+  uid?: string;
+  hloModuleName?: string;
+  hloOpName?: string;
+}
+
+// Type guard for the 'EntrySelected' custom event.
+function isEntrySelectedEvent(
+    event: Event,
+    ): event is CustomEvent<EntrySelectedEventDetail> {
+  return (
+      event instanceof CustomEvent && event.detail &&
+      typeof event.detail.eventIndex === 'number' &&
+      typeof event.detail.name === 'string' &&
+      typeof event.detail.startUs === 'number' &&
+      typeof event.detail.durationUs === 'number' &&
+      typeof event.detail.startUsFormatted === 'string' &&
+      typeof event.detail.durationUsFormatted === 'string');
+}
 
 /**
  * The interface for a selected event.
@@ -14,6 +58,17 @@ export interface SelectedEvent {
   name: string;
   startUsFormatted?: string;
   durationUsFormatted?: string;
+  stackTraceLinkHtml?: string;
+  rooflineModelLinkHtml?: string;
+  graphViewerLinkHtml?: string;
+}
+
+/**
+ * The interface for selected event property.
+ */
+export interface SelectedEventProperty {
+  property: string;
+  value: string|undefined;
 }
 
 // The tutorials to display while the trace viewer is loading.
@@ -43,6 +98,12 @@ function isLoadingStatusUpdateEvent(
       Object.values(TraceViewerV2LoadingStatus).includes(event.detail.status));
 }
 
+declare interface TrackView extends Element {
+  onEndPanScan_(event: Event): Function;
+  onEndSelection_(event: Event): Function;
+  onEndZoom_(event: Event): Function;
+}
+
 /** A trace viewer container component. */
 @Component({
   standalone: true,
@@ -54,14 +115,26 @@ function isLoadingStatusUpdateEvent(
     MatIconModule,
     MatProgressBarModule,
     PipesModule,
+    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatProgressSpinnerModule,
+    MatTableModule,
   ],
 })
-export class TraceViewerContainer implements OnInit, OnDestroy {
+export class TraceViewerContainer implements OnInit, OnDestroy, AfterViewInit {
   @Input() traceViewerModule: TraceViewerV2Module|null = null;
   @Input() url = '';
   @Input() useTraceViewerV2 = true;
   @Input() selectedEvent?: SelectedEvent|null;
-  @Input() isInitialLoading = false;
+  @Input() searching = false;
+  isInitialLoading = true;
+  @Input() selectedEventProperties: SelectedEventProperty[] = [];
+  @Input() eventDetailColumns: string[] = [];
+  @Output()
+  readonly eventSelected = new EventEmitter<EntrySelectedEventDetail>();
+  @Output() readonly searchEvents = new EventEmitter<SearchEventsEventDetail>();
 
   @ViewChild('tvIframe') tvIframe?: ElementRef<HTMLIFrameElement>;
 
@@ -69,6 +142,7 @@ export class TraceViewerContainer implements OnInit, OnDestroy {
   traceViewerV2LoadingStatus: TraceViewerV2LoadingStatus =
       TraceViewerV2LoadingStatus.IDLE;
   traceViewerV2ErrorMessage?: string;
+  searchQuery = '';
   search$ = new Subject<string>();
   currentSearchQuery = '';
   searchResultCountText = '';
@@ -97,6 +171,21 @@ export class TraceViewerContainer implements OnInit, OnDestroy {
         LOADING_STATUS_UPDATE_EVENT_NAME,
         this.loadingStatusUpdateEventListener,
     );
+    window.addEventListener(
+        EVENT_SELECTED_EVENT_NAME,
+        this.eventSelectedEventListener,
+    );
+    window.addEventListener(
+        SEARCH_EVENTS_EVENT_NAME,
+        this.searchEventsEventListener,
+    );
+  }
+
+  ngAfterViewInit() {
+    if (!this.useTraceViewerV2) {
+      window.addEventListener('mouseup', this.mouseUpEventListener);
+      window.addEventListener('keydown', this.keyDownEventListener);
+    }
   }
 
   ngOnDestroy() {
@@ -104,11 +193,56 @@ export class TraceViewerContainer implements OnInit, OnDestroy {
         LOADING_STATUS_UPDATE_EVENT_NAME,
         this.loadingStatusUpdateEventListener,
     );
+    window.removeEventListener(
+        EVENT_SELECTED_EVENT_NAME,
+        this.eventSelectedEventListener,
+    );
+    window.removeEventListener(
+        SEARCH_EVENTS_EVENT_NAME,
+        this.searchEventsEventListener,
+    );
+    if (!this.useTraceViewerV2) {
+      window.removeEventListener('mouseup', this.mouseUpEventListener);
+      window.removeEventListener('keydown', this.keyDownEventListener);
+    }
     // Unsubscribes all pending subscriptions.
     this.destroyed.next();
     this.destroyed.complete();
     this.stopTutorialRotation();
   }
+
+  private readonly keyDownEventListener = (event: KeyboardEvent) => {
+    // Disable hotkey listening when typing in the input box
+    const el = event.target as HTMLInputElement;
+    if (el.type === 'text') return;
+    switch (event.key) {
+      case 'a':
+      case 'd':
+      case 's':
+      case 'w':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+        this.tvIframe?.nativeElement?.focus();
+        break;
+      default:
+        break;
+    }
+  };
+
+  private readonly mouseUpEventListener = (event: Event) => {
+    const trackView: TrackView|null|undefined =
+        this.tvIframe?.nativeElement?.contentDocument?.querySelector(
+            'tr-ui-timeline-track-view',
+        );
+    try {
+      trackView?.onEndPanScan_(event);
+      trackView?.onEndSelection_(event);
+      trackView?.onEndZoom_(event);
+    } catch (e) {
+    }
+  };
 
   private readonly loadingStatusUpdateEventListener = (event: Event) => {
     if (!isLoadingStatusUpdateEvent(event)) {
@@ -122,6 +256,20 @@ export class TraceViewerContainer implements OnInit, OnDestroy {
     } else {
       this.traceViewerV2ErrorMessage = event.detail.message;
     }
+  };
+
+  private readonly eventSelectedEventListener = (e: Event) => {
+    if (!isEntrySelectedEvent(e)) {
+      return;
+    }
+    this.eventSelected.emit(e.detail);
+  };
+
+  private readonly searchEventsEventListener = (e: Event) => {
+    if (!isSearchEventsEvent(e)) {
+      return;
+    }
+    this.searchEvents.emit(e.detail);
   };
 
   /**
@@ -142,6 +290,7 @@ export class TraceViewerContainer implements OnInit, OnDestroy {
         this.traceViewerV2LoadingStatus === TraceViewerV2LoadingStatus.ERROR) {
       // Stop the tutorial rotation when loading is finished or failed.
       this.stopTutorialRotation();
+      this.isInitialLoading = false;
     } else {
       // Start the tutorial rotation when loading is in progress.
       this.startTutorialRotation();

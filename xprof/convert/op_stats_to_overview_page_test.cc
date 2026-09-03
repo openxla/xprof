05160d65d687272881990bf887997cc3856907b9
Profiler Team <copybara-servicebot@google.com>
2026-09-03 16:23:30 -0700

    Add serialization and aggregation for High Confidence TPU Duty Cycle and Program Goodput metrics into TfOpStats and Overview Page.
    PiperOrigin-RevId: 975981650

--- xprof/convert/op_stats_to_overview_page_test.cc
+++ xprof/convert/op_stats_to_overview_page_test.cc
@@ -1,18 +1,17 @@
-/* Copyright 2026 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
+// Copyright 2026 The TensorFlow Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
 #include "xprof/convert/op_stats_to_overview_page.h"
 #include <memory>
 
@@ -70,6 +69,47 @@ TEST(OpStatsToOverviewPageTest, GenerateInferenceLatencyDataTable_AverageBug) {
 
   // Expect Average (2.0 ms), not Median (1.0 ms)
   EXPECT_DOUBLE_EQ(device_time_ms, 2.0);
+}
+
+TEST(OpStatsToOverviewPageTest, HighConfidenceTpuDutyCycle) {
+  OpStats op_stats;
+  op_stats.mutable_run_environment()->set_device_type("TPU");
+  op_stats.mutable_device_op_metrics_db()->set_busy_time_ps(70);
+  op_stats.mutable_device_op_metrics_db()->set_idle_time_ps(30);
+
+  // Set high-confidence metrics
+  op_stats.mutable_device_op_metrics_db()->set_busy_time_high_confidence_ps(60);
+  op_stats.mutable_device_op_metrics_db()->set_idle_time_high_confidence_ps(20);
+
+  OverviewPage overview_page = ConvertOpStatsToOverviewPage(op_stats);
+
+  // Still standard Duty Cycle is 70%
+  EXPECT_DOUBLE_EQ(overview_page.analysis().device_duty_cycle_percent(), 70.0);
+  // HC Duty Cycle is 60 / (60 + 20) = 75%
+  EXPECT_DOUBLE_EQ(
+      overview_page.analysis().device_duty_cycle_high_confidence_percent(),
+      75.0);
+  EXPECT_DOUBLE_EQ(overview_page.analysis().idle_time_high_confidence_ps(),
+                   20.0);
+
+  auto data_table = GenerateAnalysisResultDataTable(overview_page);
+  ASSERT_NE(data_table, nullptr);
+  EXPECT_TRUE(data_table->GetCustomProperties().contains(
+      "device_duty_cycle_high_confidence_percent"));
+  EXPECT_TRUE(data_table->GetCustomProperties().contains(
+      "idle_time_high_confidence_ps"));
+}
+
+TEST(OpStatsToOverviewPageTest, HighConfidenceZeroTimeTest) {
+  OpStats op_stats;
+  op_stats.mutable_run_environment()->set_device_type("TPU");
+  op_stats.mutable_device_op_metrics_db()->set_busy_time_high_confidence_ps(0);
+  op_stats.mutable_device_op_metrics_db()->set_idle_time_high_confidence_ps(0);
+
+  OverviewPage overview_page = ConvertOpStatsToOverviewPage(op_stats);
+  EXPECT_DOUBLE_EQ(
+      overview_page.analysis().device_duty_cycle_high_confidence_percent(),
+      0.0);
 }
 TEST(OpStatsToOverviewPageTest, RooflineMetrics) {
   OpStats op_stats;
